fd202ebb0fb85b6e82a2b82052184d88b744f328
Hélène Tilquin <58738324+htilquin@users.noreply.github.com>
2023-03-22 17:08:43 +0100

    v0.3

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,16 @@ from utils_app import *
 
 
 st.set_page_config(
-    page_title="Fresco Maker", 
-    page_icon="🖼️", 
-    layout='centered', 
-    initial_sidebar_state='auto', 
-    menu_items={'About': """### Fresco Maker 
+    page_title="Fresco Maker",
+    page_icon="🖼️",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items={
+        "About": """### Fresco Maker 
     \nThis app has been created to help you make a wonderful fresco out of any picture.
     \n ---
-    \n Face detection : HOG based detector from Dlib."""}
+    \n Face detection : HOG based detector from Dlib."""
+    },
 )
 
 # TO DO : si un seul visage détecté : pas besoin de sélectionner
@@ -23,13 +25,16 @@ start_timer = start_timer()
 
 ### SIDEBAR
 st.sidebar.markdown("# File")
-uploaded_picture = st.sidebar.file_uploader("Choose a picture", type=['png', 'jpg', 'jpeg'], )
+uploaded_picture = st.sidebar.file_uploader(
+    "Choose a picture",
+    type=["png", "jpg", "jpeg"],
+)
 
 st.markdown("## Fresco Maker !")
-if uploaded_picture is None :
+if uploaded_picture is None:
     st.write("⟵ Upload a picture using the sidebar :)")
 
-else :
+else:
     # get all the faces from the picture
     detector = dlib.get_frontal_face_detector()
     image = read_image_from_streamlit_cv2(uploaded_picture)
@@ -38,49 +43,56 @@ else :
 
     full_pic = st.checkbox("Use full picture ?")
 
-    if not full_pic and len(faces) == 0 :
+    if not full_pic and len(faces) == 0:
         caption = "No face detected..."
         st.image(image, caption)
 
-    else :
+    else:
         if full_pic:
             face_images = [image_clean]
             chosen_ones = [0]
         else:
             face_images = []
-            for face_rect in faces :
+            for face_rect in faces:
                 # make rectangle wider
                 wider_face_rect = get_wider_rectangle(face_rect)
-                face_image = image_clean[wider_face_rect.top(): wider_face_rect.bottom(), wider_face_rect.left(): wider_face_rect.right()].copy()
+                face_image = image_clean[
+                    wider_face_rect.top() : wider_face_rect.bottom(),
+                    wider_face_rect.left() : wider_face_rect.right(),
+                ].copy()
                 face_images.append(face_image)
 
             st.write("Pick or unpick the faces you want to see in the frame:")
             chosen_ones = choose_faces(face_images, 6)
 
             if len(chosen_ones) == 0:
-                st.write('Pick at least one picture !')
+                st.write("Pick at least one picture !")
 
         st.sidebar.markdown("# Options")
-        symetry = st.sidebar.checkbox('Use the power of symetry')
+        symetry = st.sidebar.checkbox("Use the power of symetry")
 
         images_frieze = motif_maker(face_images, chosen_ones, symetry)
 
-        st.write("⟵ Twick the options using the sidebar :)")
+        st.write("⟵ Twitch the options using the sidebar :)")
 
-        repetition = st.sidebar.slider("Times the frieze is repeted in the 'frame':", min_value=1, max_value=20, value=5)
-        make_round = st.sidebar.checkbox('Make it round !')
+        make_round = st.sidebar.checkbox("Make it round !")
+        repetition = st.sidebar.slider(
+            "Times the frieze is repeted in the 'frame':",
+            min_value=1,
+            max_value=50,
+            value=5,
+        )
 
         # let's make a fresque !!!
-        if images_frieze :
+        if images_frieze:
             if make_round:
-                st.write('Soooooooooooonnnnnnnnnnnnnnnnnnn')
-                fresque_totale = add_frame_round(image, repetition)
+                fresque_totale = add_frame_round(image, images_frieze, repetition)
 
-            if not make_round :
+            if not make_round:
                 fresque_totale = add_frame(image, images_frieze, repetition)
             st.image(fresque_totale)
 
 
-st.markdown(footer,unsafe_allow_html=True)
+st.markdown(footer, unsafe_allow_html=True)
 
 st.write(f"Total loading time : {stop_timer(start_timer)} sec.")
--- utils_app.py
+++ utils_app.py
@@ -1,37 +1,42 @@
-import dlib
-import joblib
-import numpy as np
 import cv2
+import dlib
+import math
+import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw
-
+from PIL import Image, ImageDraw, ImageFont
 from time import time
+
 
 def start_timer():
     return time()
 
+
 def stop_timer(start_time):
-    return round(time()-start_time, 2)
+    return round(time() - start_time, 2)
+
 
 @st.cache(suppress_st_warning=True)
 def get_face_detector():
     # HOG based detector from dlib
     return dlib.get_frontal_face_detector()
 
-def detect_faces(image, detector) :
+
+def detect_faces(image, detector):
     face_rectangles = detector(image, 1)
     return face_rectangles
+
 
 def get_wider_rectangle(rect):
     width = rect.right() - rect.left()
     height = rect.bottom() - rect.top()
     new_rect = dlib.rectangle(
-        max(0, int(rect.left() - .05*width)), 
-        max(0, int(rect.top() - 0.2*height)), 
-        int(rect.right() + .05*width), 
-        int(rect.bottom() + 0.2*height))
+        max(0, int(rect.left() - 0.05 * width)),
+        max(0, int(rect.top() - 0.2 * height)),
+        int(rect.right() + 0.05 * width),
+        int(rect.bottom() + 0.2 * height),
+    )
     return new_rect
+
 
 def read_image_from_streamlit_cv2(streamlit_image):
     image = cv2.imdecode(np.frombuffer(streamlit_image.read(), np.uint8), 1)
@@ -41,19 +46,19 @@ def read_image_from_streamlit_cv2(streamlit_image):
 def choose_faces(images, columns):
     image_number = 0
     chosen_ones = []
-    for _ in range(int(len(images)/columns)+1) :
+    for _ in range(int(len(images) / columns) + 1):
         cols = st.columns(columns)
-        for column in range(0,columns):
-            if image_number >= len(images) :
+        for column in range(0, columns):
+            if image_number >= len(images):
                 break
-            else :
+            else:
                 with cols[column]:
                     st.image(images[image_number], use_column_width=True)
                     chosen = st.checkbox(f"{image_number}", value=True)
-                    if chosen :
+                    if chosen:
                         chosen_ones.append(image_number)
                     image_number += 1
-    
+
     return chosen_ones
 
 
@@ -71,24 +76,27 @@ def motif_maker(images, chosen_ones, symetry):
     if symetry:
         images_frieze_flipped = tuple(
             (
-                cv2.flip(cv2.resize(images[i], dimensions, interpolation=cv2.INTER_LINEAR), 1)
-                for i in chosen_ones#[::-1]
+                cv2.flip(
+                    cv2.resize(images[i], dimensions, interpolation=cv2.INTER_LINEAR), 1
+                )
+                for i in chosen_ones  # [::-1]
             )
         )
 
         images_frieze = images_frieze + images_frieze_flipped
 
     return images_frieze
-    
+
+
 def add_frame(image, images_frieze, repetition):
-    motif_top = np.concatenate(images_frieze*repetition, axis=1)
-    motif_b = np.concatenate(images_frieze[::-1]*repetition, axis=1)
+    motif_top = np.concatenate(images_frieze * repetition, axis=1)
+    motif_b = np.concatenate(images_frieze[::-1] * repetition, axis=1)
 
     height_original, width_original, _ = image.shape
     height_motif_top, width_motif_top, _ = motif_top.shape
     ratio_motif_top = width_motif_top / height_motif_top
-    
-    dimensions = (width_original, round(width_original/ratio_motif_top) )
+
+    dimensions = (width_original, round(width_original / ratio_motif_top))
     frise_top = cv2.resize(motif_top, dimensions, interpolation=cv2.INTER_LINEAR)
     frise_b = cv2.resize(motif_b, dimensions, interpolation=cv2.INTER_LINEAR)
 
@@ -97,43 +105,122 @@ def add_frame(image, images_frieze, repetition):
     height_fresque_tb, width_fresque_tb, _ = fresque_top_bottom.shape
     height_frise_top, width_frise_top, _ = frise_top.shape
 
-    nb_motif_v = round((height_fresque_tb / height_frise_top)/len(images_frieze))
+    nb_motif_v = round((height_fresque_tb / height_frise_top) / len(images_frieze))
 
-    motif_left = np.concatenate(images_frieze[::-1]*nb_motif_v, axis=0)
-    motif_right = np.concatenate(images_frieze*nb_motif_v, axis=0)
+    motif_left = np.concatenate(images_frieze[::-1] * nb_motif_v, axis=0)
+    motif_right = np.concatenate(images_frieze * nb_motif_v, axis=0)
 
     height_motif_v, width_motif_v, _ = motif_left.shape
     ratio_motif_v = width_motif_v / height_motif_v
 
-    dimensions = (round(height_fresque_tb*ratio_motif_v), height_fresque_tb )
+    dimensions = (round(height_fresque_tb * ratio_motif_v), height_fresque_tb)
     frise_left = cv2.resize(motif_left, dimensions, interpolation=cv2.INTER_LINEAR)
     frise_right = cv2.resize(motif_right, dimensions, interpolation=cv2.INTER_LINEAR)
 
-    fresque_totale = np.concatenate((frise_left, fresque_top_bottom, frise_right), axis=1)
+    fresque_totale = np.concatenate(
+        (frise_left, fresque_top_bottom, frise_right), axis=1
+    )
     return fresque_totale
 
 
-def add_frame_round(image, repetition):
+def image_to_square(image):
     height, width, _ = image.shape
-    mini_dimension = min(height, width)
-    image_square = image[0:mini_dimension, 0:mini_dimension]
-    lum_img = Image.new('L', [mini_dimension, mini_dimension], 0)
+    square_dim = min(height, width)
+
+    image_square = image[
+        int((height - square_dim) / 2) : int(height - (height - square_dim) / 2),
+        int((width - square_dim) / 2) : int(width - (width - square_dim) / 2),
+    ]
+
+    return image_square, square_dim
+
+
+def calculate_circle_points(r, nb_pts, coeff, center, mini_h, mini_w):
+    points = []
+    for i in range(nb_pts):
+        points.append(
+            [
+                int(coeff * r * math.sin((i * 2 * math.pi) / nb_pts)) + center[1],
+                int(coeff * r * math.cos((i * 2 * math.pi) / nb_pts)) + center[1],
+            ]
+        )
+    return points
+
+
+def add_frame_round(image, images_frieze, repetition):
+    image_square, square_dim = image_to_square(image)
+
+    mini_h, mini_w, _ = images_frieze[0].shape
+    big_dim = square_dim + mini_h * 2
+
+    background = Image.new("RGB", (big_dim, big_dim), "white")
+
+    offset = ((big_dim - square_dim) // 2, (big_dim - square_dim) // 2)
+    background.paste(Image.fromarray(image_square), offset)
+
+    center = (offset[0] + square_dim // 2, offset[1] + square_dim // 2)
+
+    angles = [int(i / repetition * 360) for i in range(repetition)]
+    rotate = st.sidebar.checkbox("Rotate pictures in frame ?", value=True)
+    coeff = st.sidebar.slider("Size of circle", 0, 100, 75)
+    size = st.sidebar.slider(
+        "Size of the picture", min_value=10, max_value=100, value=85
+    )
+    radius = (big_dim) // 2
+    width = st.sidebar.slider(
+        "Horizontal shifting", min_value=-100, max_value=100, value=0
+    )
+    height = st.sidebar.slider(
+        "Vertical shifting", min_value=-100, max_value=100, value=0
+    )
+    coordinates = calculate_circle_points(
+        radius, repetition, coeff / 100, center, mini_h // 100, size * mini_w // 100
+    )
+
+    friezes = [
+        Image.fromarray(images_frieze[i % len(images_frieze)]).convert("RGBA")
+        for i in range(repetition)
+    ]
+
+    for i in range(repetition):
+        img = friezes[i].resize(
+            (size * mini_w // 100, size * mini_h // 100), Image.LANCZOS
+        )
+        if rotate:
+            img = img.rotate(angles[i], expand=True)
+        coordinates_modif = (
+            coordinates[i][0] + width * 5,
+            coordinates[i][1] + height * 5,
+        )
+        background.paste(img, coordinates_modif, img)
+
+    text = ImageDraw.Draw(background)
+    text.text(
+        center,
+        text="x",
+        fill="red",
+        anchor="mm",
+    )
+
+    lum_img = Image.new("L", [big_dim, big_dim], 0)
 
     draw = ImageDraw.Draw(lum_img)
-    draw.pieslice([(0,0), (mini_dimension, mini_dimension)], 0, 360,
-                    fill = 255, outline = "white")
-    
-    img_arr = np.array(image_square)
+    draw.pieslice(
+        [(0, 0), (big_dim, big_dim)],
+        0,
+        360,
+        fill=255,
+        # outline="white"
+    )
+
+    img_arr = np.array(background)
     lum_img_arr = np.array(lum_img)
-    final_img_arr = np.dstack((img_arr, lum_img_arr))
+    round_img_arr = np.dstack((img_arr, lum_img_arr))
 
-    return final_img_arr
-
-
+    return round_img_arr
 
 
-
-footer="""<style>
+footer = """<style>
 a:link , a:visited{
 color: red;
 background-color: transparent;
